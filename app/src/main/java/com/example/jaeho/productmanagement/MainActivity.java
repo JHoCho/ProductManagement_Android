d9de149ca780c48f969c2f254a54c4301fd1ad08
JAEHO <chojaeho1122@gmail.com>
2017-07-20 14:11:39 +0900

    카메라 퍼미션 완료!

--- app/src/main/java/com/example/jaeho/productmanagement/MainActivity.java
+++ app/src/main/java/com/example/jaeho/productmanagement/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jaeho.productmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
         edtID = (EditText) findViewById(R.id.edtID);
         edtPW = (EditText) findViewById(R.id.edtPW);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
