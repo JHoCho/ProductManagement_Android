599481a7e796feabeb6b1fc31562787cb2dea1df
JAEHO <chojaeho1122@gmail.com>
2017-08-31 13:33:58 +0900

    로케이션 선택부분 추가

---
+++ app/src/main/java/com/example/jaeho/productmanagement/View/Activities/SelectLocationActivity.java
@@ -0,0 +1,29 @@
+package com.example.jaeho.productmanagement.View.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.jaeho.productmanagement.R;
+
+public class SelectLocationActivity extends AppCompatActivity {
+    Button btnStartCheck;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_location);
+        btnStartCheck = (Button)findViewById(R.id.btnStartCheck);
+
+        btnStartCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectLocationActivity.this,SelectedItemCheckActivity.class);
+                //이부분에서 얻어온부분을 인텐트로 새 엑티비티로 넘겨주어야함.
+                startActivity(intent);
+            }
+        });
+    }
+
+}
