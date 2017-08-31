533cfb3c53515e727a6138f5967468e73b80a4c5
JAEHO <chojaeho1122@gmail.com>
2017-08-31 13:31:47 +0900

    QR이 우리가 정한 규격에 맞지않을경우 다른 QR로 인식하고 무시함

--- app/src/main/java/com/example/jaeho/productmanagement/utils/QRStringTokenizer.java
+++ app/src/main/java/com/example/jaeho/productmanagement/utils/QRStringTokenizer.java
@@ -17,18 +17,23 @@ public class QRStringTokenizer {
         splitQR(s);
     }
     public void splitQR(String s){
-        String[] result = s.split(",");
-        qrdo.setProductName(result[0]);
-        qrdo.setDetailedProductName(result[1]);
-        qrdo.setSerialNumber(result[2]);
-        qrdo.setCompanyName(result[3]);
-        qrdo.setBuilding(result[4]);
-        qrdo.setFloor(result[5]);
-        qrdo.setRoomName(result[6]);
-        qrdo.setDate(result[7]);
-        qrdo.setAdminID(result[8]);
-        qrdo.setPrice(result[9]);
-        qrdo.setLocation(qrdo.getBuilding()+qrdo.getFloor()+qrdo.getRoomName());
+        try {
+            String[] result = s.split(",");
+            qrdo.setProductName(result[0]);
+            qrdo.setDetailedProductName(result[1]);
+            qrdo.setSerialNumber(result[2]);
+            qrdo.setCompanyName(result[3]);
+            qrdo.setBuilding(result[4]);
+            qrdo.setFloor(result[5]);
+            qrdo.setRoomName(result[6]);
+            qrdo.setDate(result[7]);
+            qrdo.setAdminID(result[8]);
+            qrdo.setPrice(result[9]);
+            qrdo.setLocation(qrdo.getBuilding() + qrdo.getFloor() + qrdo.getRoomName());
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("QRStringTokenizer"+"에서 Exception발생 CheckMate에서 정의한 QR이 아닙니다.");
+            e.printStackTrace();
+        }
     }
     public QRDO getSplitedQRDO(){
         return qrdo;
